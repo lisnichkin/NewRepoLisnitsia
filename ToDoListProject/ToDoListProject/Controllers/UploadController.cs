ffc933b88775e5d1b5c21ebf38aa72f9fdba2569
lisnichkin <lisnichkin2@gmail.com>
2017-02-13 18:15:04 +0200

    add Upload file functional
    add api Upload Controller

---
+++ ToDoListProject/ToDoListProject/Controllers/UploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoListProject.DataProviders;
+using ToDoListProject.Models;
+
+namespace ToDoListProject.Controllers
+{
+    public class UploadController : ApiController
+    {
+        [HttpPost]
+        public void Post([FromBody] Task value)
+        {
+            DataProvider.SaveTaskToBase(value);
+        }
+    }
+}
